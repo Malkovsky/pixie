d999c8975a06defa1cdfd6348c0bdc510200ae32
mperikov <mperikov2@mail.ru>
2025-12-02 21:15:19 +0300

    Format fix

--- src/alignment_comparison.cpp
+++ src/alignment_comparison.cpp
@@ -1,35 +1,36 @@
 #include <benchmark/benchmark.h>
 #include <immintrin.h>
+
 #include <vector>
 
 #include "bits.h"
 
 #ifdef PIXIE_AVX512_SUPPORT
 
-static void BM_Loadu512(benchmark::State &state) {
+static void BM_Loadu512(benchmark::State& state) {
   alignas(64) uint8_t data[128];
 
-  const __m512i *ptr = reinterpret_cast<const __m512i *>(data);
+  const __m512i* ptr = reinterpret_cast<const __m512i*>(data);
 
   for (auto _ : state) {
     benchmark::DoNotOptimize(_mm256_loadu_si512(ptr));
   }
 }
 
-static void BM_Load512(benchmark::State &state) {
+static void BM_Load512(benchmark::State& state) {
   alignas(64) uint8_t data[128];
 
-  const __m512i *ptr = reinterpret_cast<const __m512i *>(data);
+  const __m512i* ptr = reinterpret_cast<const __m512i*>(data);
 
   for (auto _ : state) {
     benchmark::DoNotOptimize(_mm512_load_si512(ptr));
   }
 }
 
-static void BM_Storeu512(benchmark::State &state) {
+static void BM_Storeu512(benchmark::State& state) {
   alignas(64) uint8_t data[128];
   __m512i value = _mm512_setzero_si512();
-  __m512i *ptr = reinterpret_cast<__m512i *>(data);
+  __m512i* ptr = reinterpret_cast<__m512i*>(data);
 
   for (auto _ : state) {
     _mm512_storeu_si512(ptr, value);
@@ -37,10 +38,10 @@ static void BM_Storeu512(benchmark::State &state) {
   }
 }
 
-static void BM_Store512(benchmark::State &state) {
+static void BM_Store512(benchmark::State& state) {
   alignas(64) uint8_t data[128];
   __m512i value = _mm512_setzero_si512();
-  __m512i *ptr = reinterpret_cast<__m512i *>(data);
+  __m512i* ptr = reinterpret_cast<__m512i*>(data);
 
   for (auto _ : state) {
     _mm512_store_si512(ptr, value);
@@ -55,30 +56,30 @@ BENCHMARK(BM_Store512);
 
 #else
 
-static void BM_Loadu256(benchmark::State &state) {
+static void BM_Loadu256(benchmark::State& state) {
   alignas(32) uint8_t data[64];
 
-  const __m256i *ptr = reinterpret_cast<const __m256i *>(data);
+  const __m256i* ptr = reinterpret_cast<const __m256i*>(data);
 
   for (auto _ : state) {
     benchmark::DoNotOptimize(_mm256_loadu_si256(ptr));
   }
 }
 
-static void BM_Load256(benchmark::State &state) {
+static void BM_Load256(benchmark::State& state) {
   alignas(32) uint8_t data[64];
 
-  const __m256i *ptr = reinterpret_cast<const __m256i *>(data);
+  const __m256i* ptr = reinterpret_cast<const __m256i*>(data);
 
   for (auto _ : state) {
     benchmark::DoNotOptimize(_mm256_load_si256(ptr));
   }
 }
 
-static void BM_Storeu256(benchmark::State &state) {
+static void BM_Storeu256(benchmark::State& state) {
   alignas(32) uint8_t data[64];
   __m256i value = _mm256_setzero_si256();
-  __m256i *ptr = reinterpret_cast<__m256i *>(data);
+  __m256i* ptr = reinterpret_cast<__m256i*>(data);
 
   for (auto _ : state) {
     _mm256_storeu_si256(ptr, value);
@@ -86,10 +87,10 @@ static void BM_Storeu256(benchmark::State &state) {
   }
 }
 
-static void BM_Store256(benchmark::State &state) {
+static void BM_Store256(benchmark::State& state) {
   alignas(32) uint8_t data[64];
   __m256i value = _mm256_setzero_si256();
-  __m256i *ptr = reinterpret_cast<__m256i *>(data);
+  __m256i* ptr = reinterpret_cast<__m256i*>(data);
 
   for (auto _ : state) {
     _mm256_store_si256(ptr, value);
