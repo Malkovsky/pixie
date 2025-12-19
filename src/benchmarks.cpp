01819e1c6ea403d3cc9114627a3abaa4001b503c
mperikov <mperikov2@mail.ru>
2025-12-19 15:23:06 +0300

    select0/rank0 benchmarks

--- src/benchmarks.cpp
+++ src/benchmarks.cpp
@@ -24,6 +24,22 @@ static void BM_RankNonInterleaved(benchmark::State& state) {
   }
 }
 
+static void BM_RankZeroNonInterleaved(benchmark::State& state) {
+  size_t n = state.range(0);
+  std::mt19937_64 rng(42);
+
+  std::vector<uint64_t> bits(((8 + n / 64) / 8) * 8);
+  for (auto& x : bits) {
+    x = rng();
+  }
+  pixie::BitVector bv(bits, n);
+
+  for (auto _ : state) {
+    uint64_t pos = rng() % n;
+    benchmark::DoNotOptimize(bv.rank0(pos));
+  }
+}
+
 static void BM_RankInterleaved(benchmark::State& state) {
   size_t n = state.range(0);
   std::mt19937_64 rng(42);
@@ -58,6 +74,24 @@ static void BM_SelectNonInterleaved(benchmark::State& state) {
   }
 }
 
+static void BM_SelectZeroNonInterleaved(benchmark::State& state) {
+  size_t n = state.range(0);
+  std::mt19937_64 rng(42);
+
+  std::vector<uint64_t> bits(((8 + n / 64) / 8) * 8);
+  for (auto& x : bits) {
+    x = rng();
+  }
+  pixie::BitVector bv(bits, n);
+
+  auto max_rank0 = bv.rank0(bv.size()) + 1;
+
+  for (auto _ : state) {
+    uint64_t rank0 = rng() % max_rank0;
+    benchmark::DoNotOptimize(bv.select0(rank0));
+  }
+}
+
 static void BM_RankNonInterleaved10PercentFill(benchmark::State& state) {
   size_t n = state.range(0);
   std::mt19937_64 rng(42);
@@ -74,6 +108,25 @@ static void BM_RankNonInterleaved10PercentFill(benchmark::State& state) {
   for (auto _ : state) {
     uint64_t pos = rng() % n;
     benchmark::DoNotOptimize(bv.rank(pos));
+  }
+}
+
+static void BM_RankZeroNonInterleaved10PercentFill(benchmark::State& state) {
+  size_t n = state.range(0);
+  std::mt19937_64 rng(42);
+
+  std::vector<uint64_t> bits(((8 + n / 64) / 8) * 8);
+  size_t num_ones = n * 0.1;
+  for (int i = 0; i < num_ones; i++) {
+    uint64_t pos = rng() % n;
+    bits[pos / 64] |= (1ULL << pos % 64);
+  }
+
+  pixie::BitVector bv(bits, n);
+
+  for (auto _ : state) {
+    uint64_t pos = rng() % n;
+    benchmark::DoNotOptimize(bv.rank0(pos));
   }
 }
 
@@ -98,6 +151,27 @@ static void BM_SelectNonInterleaved10PercentFill(benchmark::State& state) {
   }
 }
 
+static void BM_SelectZeroNonInterleaved10PercentFill(benchmark::State& state) {
+  size_t n = state.range(0);
+  std::mt19937_64 rng(42);
+
+  std::vector<uint64_t> bits(((8 + n / 64) / 8) * 8);
+  size_t num_ones = n * 0.1;
+  for (int i = 0; i < num_ones; i++) {
+    uint64_t pos = rng() % n;
+    bits[pos / 64] |= (1ULL << pos % 64);
+  }
+
+  pixie::BitVector bv(bits, n);
+
+  auto max_rank0 = bv.rank0(bv.size()) + 1;
+
+  for (auto _ : state) {
+    uint64_t rank0 = rng() % max_rank0;
+    benchmark::DoNotOptimize(bv.select0(rank0));
+  }
+}
+
 static void BM_RankNonInterleaved90PercentFill(benchmark::State& state) {
   size_t n = state.range(0);
   std::mt19937_64 rng(42);
@@ -114,6 +188,25 @@ static void BM_RankNonInterleaved90PercentFill(benchmark::State& state) {
   for (auto _ : state) {
     uint64_t pos = rng() % n;
     benchmark::DoNotOptimize(bv.rank(pos));
+  }
+}
+
+static void BM_RankZeroNonInterleaved90PercentFill(benchmark::State& state) {
+  size_t n = state.range(0);
+  std::mt19937_64 rng(42);
+
+  std::vector<uint64_t> bits(((8 + n / 64) / 8) * 8);
+  size_t num_ones = n * 0.9;
+  for (int i = 0; i < num_ones; i++) {
+    uint64_t pos = rng() % n;
+    bits[pos / 64] |= (1ULL << pos % 64);
+  }
+
+  pixie::BitVector bv(bits, n);
+
+  for (auto _ : state) {
+    uint64_t pos = rng() % n;
+    benchmark::DoNotOptimize(bv.rank0(pos));
   }
 }
 
@@ -138,13 +231,40 @@ static void BM_SelectNonInterleaved90PercentFill(benchmark::State& state) {
   }
 }
 
+static void BM_SelectZeroNonInterleaved90PercentFill(benchmark::State& state) {
+  size_t n = state.range(0);
+  std::mt19937_64 rng(42);
+
+  std::vector<uint64_t> bits(((8 + n / 64) / 8) * 8);
+  size_t num_ones = n * 0.9;
+  for (int i = 0; i < num_ones; i++) {
+    uint64_t pos = rng() % n;
+    bits[pos / 64] |= (1ULL << pos % 64);
+  }
+
+  pixie::BitVector bv(bits, n);
+
+  auto max_rank0 = bv.rank0(bv.size()) + 1;
+
+  for (auto _ : state) {
+    uint64_t rank0 = rng() % max_rank0;
+    benchmark::DoNotOptimize(bv.select(rank0));
+  }
+}
+
+BENCHMARK(BM_RankInterleaved)
+    ->ArgNames({"n"})
+    ->RangeMultiplier(4)
+    ->Range(8, 1ull << 34)
+    ->Iterations(100000);
+
 BENCHMARK(BM_RankNonInterleaved)
     ->ArgNames({"n"})
     ->RangeMultiplier(4)
     ->Range(8, 1ull << 34)
     ->Iterations(100000);
 
-BENCHMARK(BM_RankInterleaved)
+BENCHMARK(BM_RankZeroNonInterleaved)
     ->ArgNames({"n"})
     ->RangeMultiplier(4)
     ->Range(8, 1ull << 34)
@@ -156,7 +276,19 @@ BENCHMARK(BM_SelectNonInterleaved)
     ->Range(8, 1ull << 34)
     ->Iterations(100000);
 
+BENCHMARK(BM_SelectZeroNonInterleaved)
+    ->ArgNames({"n"})
+    ->RangeMultiplier(4)
+    ->Range(8, 1ull << 34)
+    ->Iterations(100000);
+
 BENCHMARK(BM_RankNonInterleaved10PercentFill)
+    ->ArgNames({"n"})
+    ->RangeMultiplier(4)
+    ->Range(8, 1ull << 34)
+    ->Iterations(100000);
+
+BENCHMARK(BM_RankZeroNonInterleaved10PercentFill)
     ->ArgNames({"n"})
     ->RangeMultiplier(4)
     ->Range(8, 1ull << 34)
@@ -168,13 +300,31 @@ BENCHMARK(BM_SelectNonInterleaved10PercentFill)
     ->Range(8, 1ull << 34)
     ->Iterations(100000);
 
+BENCHMARK(BM_SelectZeroNonInterleaved10PercentFill)
+    ->ArgNames({"n"})
+    ->RangeMultiplier(4)
+    ->Range(8, 1ull << 34)
+    ->Iterations(100000);
+
 BENCHMARK(BM_RankNonInterleaved90PercentFill)
     ->ArgNames({"n"})
     ->RangeMultiplier(4)
     ->Range(8, 1ull << 34)
     ->Iterations(100000);
 
+BENCHMARK(BM_RankZeroNonInterleaved90PercentFill)
+    ->ArgNames({"n"})
+    ->RangeMultiplier(4)
+    ->Range(8, 1ull << 34)
+    ->Iterations(100000);
+
 BENCHMARK(BM_SelectNonInterleaved90PercentFill)
+    ->ArgNames({"n"})
+    ->RangeMultiplier(4)
+    ->Range(8, 1ull << 34)
+    ->Iterations(100000);
+
+BENCHMARK(BM_SelectZeroNonInterleaved90PercentFill)
     ->ArgNames({"n"})
     ->RangeMultiplier(4)
     ->Range(8, 1ull << 34)
